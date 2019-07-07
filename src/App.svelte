05b16cdcbcb1125be2808036d8c3fbc34d7f2273
Tobi <tobiasoberbeck@gmx.de>
2019-07-07 11:04:49 +0200

    images injected into html. Auto opening of readme (fully working)

--- src/App.svelte
+++ src/App.svelte
@@ -43,9 +43,21 @@
     }
   ];
 
-  let selectedBookName = ""; // books[0];
+  let selectedBookName = "";
   let selectedBookIndex = 0;
   let selectedChapterFileName = "README.md";
+
+  // request on application start
+  selectFirstBook();
+  requestBookFolderContentsOfSelectedBook();
+
+  // insert dummy data
+  // for (let i = 0; i < books.length; i++) {
+  //   const book = books[i];
+  //   book.mdFileNames.push(Math.random());
+  //   book.mdFileNames.push(Math.random());
+  //   book.mdFileNames.push(Math.random());
+  // }
 
   function onChapterSelectionChanged(selectEvent) {
     selectChapter(selectEvent);
@@ -54,6 +66,14 @@
 
   function selectChapter(selectEvent) {
     const selectedChapterIndex = selectEvent.target.selectedIndex;
+    selectChapterByIndex(selectedChapterIndex);
+  }
+
+  function selectFirstChapter() {
+    selectChapterByIndex(0);
+  }
+
+  function selectChapterByIndex(selectedChapterIndex) {
     selectedChapterFileName =
       books[selectedBookIndex].mdFileNames[selectedChapterIndex];
   }
@@ -64,20 +84,35 @@
     }
 
     const encodedBookName = encodeURIComponent(selectedBookName);
-    const fileRequestUrl = buildFileRequestUrl(
+    const fileRequestUrl = buildFileUrl(
       encodedBookName,
       selectedChapterFileName
     );
     axios
       .get(fileRequestUrl)
       .then(chapterFileResponse => {
-        const html = marked(chapterFileResponse.data);
+        let html = marked(chapterFileResponse.data);
+        html = injectImageUrlsIntoHtml(html, encodedBookName);
         const contentElement = document.getElementById("content");
         contentElement.innerHTML = html;
       })
       .catch(function(error) {
         console.log(error);
       });
+  }
+
+  function buildFileUrl(encodedBookName, fileName) {
+    return RAW_REPO_BASE_URL + encodedBookName + "/" + fileName;
+  }
+
+  function injectImageUrlsIntoHtml(html, encodedBookName) {
+    const imageFileNames = books[selectedBookIndex].imageFileNames;
+
+    for (var j = 0; j < imageFileNames.length; j++) {
+      var imageUrl = buildFileUrl(encodedBookName, imageFileNames[j]);
+      html = html.replace(imageFileNames[j], imageUrl);
+    }
+    return html;
   }
 
   function onBookSelectionChanged(selectEvent) {
@@ -87,6 +122,15 @@
 
   function selectBook(selectEvent) {
     selectedBookIndex = selectEvent.target.selectedIndex;
+    selectBookByIndex(selectedBookIndex);
+  }
+
+  function selectFirstBook() {
+    selectedBookIndex = 0;
+    selectBookByIndex(0);
+  }
+
+  function selectBookByIndex(selectedBookIndex) {
     selectedBookName = books[selectedBookIndex].name;
   }
 
@@ -110,14 +154,13 @@
         );
 
         cacheImageAndSortedmdFileNames(imageFileNames, sortedMarkdownFiles);
+
+        selectFirstChapter();
+        requestMarkdownFile();
       })
       .catch(function(error) {
         console.log(error);
       });
-  }
-
-  function buildFileRequestUrl(encodedBookName, chapterFilename) {
-    return RAW_REPO_BASE_URL + encodedBookName + "/" + chapterFilename;
   }
 
   function extractImageAndmdFileNames(folderContentsResponse) {
@@ -153,16 +196,16 @@
     return mdFilesSortedByToc;
   }
 
+  function filterFilesByFileName(markdownFiles, searchFileName) {
+    const temp = markdownFiles.filter(
+      fileName => fileName.toLowerCase().indexOf(searchFileName) !== -1
+    );
+    return temp.sort(); // sort numbered files (nameINDEX.md)
+  }
+
   function cacheImageAndSortedmdFileNames(imageFileNames, sortedMdFileNames) {
     books[selectedBookIndex].imageFileNames = imageFileNames;
     books[selectedBookIndex].mdFileNames = sortedMdFileNames;
-  }
-
-  function filterFilesByFileName(markdownFiles, searchFileName) {
-    const temp = markdownFiles.filter(
-      fileName => fileName.indexOf(searchFileName) !== -1
-    );
-    return temp.sort(); // sort numbered files (nameINDEX.md)
   }
 </script>
 
@@ -171,8 +214,6 @@
 </style>
 
 <div>
-  <p class="text-xl">-> {selectedBookName}</p>
-
   <select
     on:change={onBookSelectionChanged}
     class="m-2 border-solid border-blue-200 border-2">
@@ -182,7 +223,7 @@
   </select>
   <br />
 
-  {#if books[selectedBookIndex].mdFileNames.length}
+  {#if books[selectedBookIndex].mdFileNames.length > 0}
     <select
       on:change={onChapterSelectionChanged}
       class="m-2 border-solid border-blue-200 border-2">
@@ -192,5 +233,30 @@
     </select>
   {/if}
 
-  <main id="content" class="markdown-body">asd</main>
+  <main class="bg-white m-4 p-4 rounded-lg">
+    <div id="content" class="markdown-body">
+      Helping marketers serve unmatched cross-phase personalized experiences at
+      every step of the FirstSpriti Digital Experience Platform powers
+      enterprise-class. Clicking on this link which refers to B2B Marketing
+      awards shortlist will take you to the awards page of the FirstSpriti
+      Digital Experience Platform. Spriti introduced new capabilities of the
+      FirstSpriti Digital Experience Platform. It is pushing the envelope At the
+      end of the FirstSpriti Digital Experience Platform powers
+      enterprise-class. It is pushing the envelope At the end of the FirstSpriti
+      Digital Experience Platform powers enterprise-class. Helping marketers
+      serve unmatched cross-phase personalized experiences at every step of the
+      FirstSpriti Digital Experience Platform powers enterprise-class. Clicking
+      on this link which refers to B2B Marketing awards shortlist will take you
+      to the awards page of the customer journey. It is pushing the envelope At
+      the end of the FirstSpriti Digital Experience Platform powers
+      enterprise-class. Clicking on this link which refers to B2B Marketing
+      awards shortlist will take you to the awards page of the customer journey.
+      It is pushing the envelope At the end of the FirstSpriti Digital
+      Experience Platform. These innovations help CMOs challenged with the
+      delivery of omnichannel digital experiences for some of the FirstSpriti
+      Digital Experience Platform powers enterprise-class. Spriti introduced new
+      capabilities of the FirstSpriti Digital Experience Platform powers
+      enterprise-class.
+    </div>
+  </main>
 </div>
